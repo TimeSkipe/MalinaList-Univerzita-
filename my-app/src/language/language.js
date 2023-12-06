2fe6d5762706567c0812b555a6d57ece7ad934c6
TimeSkipe <126266923+TimeSkipe@users.noreply.github.com>
2023-12-06 17:54:10 +0100

    Create language.js

---
+++ my-app/src/language/language.js
@@ -0,0 +1,160 @@
+const language = {
+    English:{
+        header:{
+            Search:"Please write your list`s name",
+            Language:"Language",
+            LogOut:'Log out',
+            Register:'Register',
+            Login:"Login",
+            EnglishLan:'English',
+            CzechLan:'Czech',
+            UkrainianLan:"Ukrainian",
+            thema:'Thema'
+        },
+        Lists:{
+            CreateNewList:'Create new list',
+            Lists:'Lists',
+            Admin:'Admin',
+            Member:'Member'
+        },
+        EditList:{
+            EditList:'Edit list',
+            ListName:"List`s name",
+            Save:'Save changes'
+        },
+        ListCreate:{
+            CreateList:'Create list',
+            ListName:"List`s name",
+            CreateNewList:"Create new list"
+        },
+        List:{
+            AddNewTowar:"Add new towar",
+            MemberList:"Member list",
+            MemberToAdd:"Member to add",
+            AddMemberBut:"Add new member",
+            Save:'Save',
+            Leave:"Leave from list",
+            TitleItem:"Title item",
+            ItemDes:"Item description",
+        },
+        Register:{
+            Register:'Register',
+            Name:"Name",
+            Email:"Email",
+            Password:"Password",
+            Send:"Send"
+        },
+        Login:{
+            Login:'Login',
+            Email:"Email",
+            Password:"Password",
+            Send:"Send"
+        }
+    },
+    Czech:{
+        header:{
+            Search:"Napište název seznamu",
+            Language:"Jazyk",
+            LogOut:'Odhlášení',
+            Register:'Registrace',
+            Login:"Přihlášení",
+            EnglishLan:'Angličtina',
+            CzechLan:'Česky',
+            UkrainianLan:"Ukrajinský",
+            thema:'Tema'
+        },
+        Lists:{
+            CreateNewList:'Vytvořit nový seznam',
+            Lists:'Seznamy',
+            Admin:'Vlastník',
+            Member:'Člen'
+        },
+        EditList:{
+            EditList:'Upravit seznam',
+            ListName:"Název seznamu",
+            Save:'Uložení změn'
+        },
+        ListCreate:{
+            CreateList:'Vytvořit seznam',
+            ListName:"Název seznamu",
+            CreateNewList:"Vztvořit nový seznam"
+        },
+        List:{
+            AddNewTowar:"Přidat nový zboží",
+            MemberList:"Seznam členů",
+            MemberToAdd:"Člen, který se přidá",
+            AddMemberBut:"Přidání nového člena",
+            Save:'Uložit',
+            Leave:"Opustit seznam",
+            TitleItem:"Název položky",
+            ItemDes:"Popis položky",
+        },
+        Register:{
+            Register:'Registrace',
+            Name:"Jméno",
+            Email:"Email",
+            Password:"Heslo",
+            Send:"Odeslat"
+        },
+        Login:{
+            Login:'Přihlášení',
+            Email:"Email",
+            Password:"Heslo",
+            Send:"Odeslat"
+        }
+    },
+    Ukrainian:{
+        header:{
+            Search:"Будь ласка напишіть назву списку",
+            Language:"Мова",
+            LogOut:'Вийти',
+            Register:'Реєстрація',
+            Login:"Авторизація",
+            EnglishLan:'Англійська',
+            CzechLan:'Чеська',
+            UkrainianLan:"Українська",
+            thema:'Тема'
+        },
+        Lists:{
+            CreateNewList:'Створити новий список',
+            Lists:'Списки',
+            Admin:'Власник',
+            Member:'Учасник'
+        },
+        EditList:{
+            EditList:'Редагувати список',
+            ListName:"Назва списку",
+            Save:'Зберегти зміни'
+        },
+        ListCreate:{
+            CreateList:'Створити список',
+            ListName:"Назва списку",
+            CreateNewList:"Створити новий список"
+        },
+        List:{
+            AddNewTowar:"Додати новий товар",
+            MemberList:"Списко учасників",
+            MemberToAdd:"Потенційні учасники",
+            AddMemberBut:"Додати нового учасника",
+            Save:'Зберегти',
+            Leave:"Покинути список",
+            TitleItem:"Назва товару",
+            ItemDes:"Коментар до товару",
+        },
+        Register:{
+            Register:'Реєстрація',
+            Name:"Ім'я",
+            Email:"Email пошта",
+            Password:"Пароль",
+            Send:"Надіслати"
+        },
+        Login:{
+            Login:'Авторизація',
+            Email:"Email пошта",
+            Password:"Пароль",
+            Send:"Надіслати"
+        }
+    }
+}
+
+export default language;
